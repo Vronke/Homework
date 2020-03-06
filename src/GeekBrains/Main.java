ee0bc10001437ed5d3ab6fb78781ce23120a8c90
s.petranin <s-petranin@mail.ru>
2020-03-06 13:27:29 +0300

    Homework #5

--- src/GeekBrains/Main.java
+++ src/GeekBrains/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         Person[] personArr = new Person[5];
+
         personArr[0] = new Person("Человек1", "Должность1", "человек1@почта.ру", "+7 (911) 111-11-11", 100000, 45);
         personArr[1] = new Person("Человек2", "Должность2", "человек2@почта.ру", "+7 (922) 222-22-22", 30000, 24);
         personArr[2] = new Person("Человек3", "Должность3", "человек3@почта.ру", "+7 (933) 333-33-33", 35000, 32);
