e841af7b1562dbe1f796fe0ae3a0379e00801e9f
s.petranin <s-petranin@mail.ru>
2020-03-11 15:40:11 +0300

    Homework #7

--- src/GeekBrains/Dog.java
+++
@@ -1,9 +0,0 @@
-package GeekBrains;
-
-import java.util.Random;
-
-public class Dog extends Animal {
-    Dog() {
-        super(500, 0.5, 10);
-    }
-}
--- src/GeekBrains/Main.java
+++ src/GeekBrains/Main.java
@@ -6,10 +6,31 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Cat cat = new Cat();
-        cat.swim(0);
-        System.out.println(cat.swim(0));
-        Dog dog = new Dog();
-        System.out.println(dog.run(100));
+        Cat cat1 = new Cat("Barsik", 5);
+        Cat cat2 = new Cat("Talka", 7);
+        Cat cat3 = new Cat("Sega", 9);
+        Cat cat4 = new Cat("Pin", 12);
+        Cat cat5 = new Cat("Leva", 8);
+        Cat[] cats = {cat1, cat2, cat3, cat4, cat5};
+
+        Plate plate = new Plate(30);
+        plate.info();
+
+        for (Cat cat : cats) {
+            if (!cat.isSatiety)
+                cat.eat(plate);
+            cat.info();
+        }
+        plate.info();
+
+        plate.increaseFood(15);
+
+        for (Cat cat : cats) {
+            if (!cat.isSatiety)
+                cat.eat(plate);
+            cat.info();
+        }
+        plate.info();
     }
 }
+
